fde02e29b8b1b9f38caee5673e44db1a5685b1c5
tom <thaaster@avans.nl>
2018-01-16 13:29:58 +0100

    kan nu personen uit database halen en filteren

---
+++ src/app/factuur/factuur-detail/factuur-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-factuur-detail',
+  templateUrl: './factuur-detail.component.html',
+  styleUrls: ['./factuur-detail.component.css']
+})
+export class FactuurDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
