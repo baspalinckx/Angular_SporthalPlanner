68217c154fa0d5cbeb937a932af47e91fcdd8e8c
baspalinckx <bas.palinckx@hotmail.nl>
2018-01-16 14:36:25 +0100

    resultaat component

---
+++ src/app/factuur/factuur-resultaat/factuur-resultaat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-factuur-resultaat',
+  templateUrl: './factuur-resultaat.component.html',
+  styleUrls: ['./factuur-resultaat.component.css']
+})
+export class FactuurResultaatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
