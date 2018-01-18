0a3eca74b3575a5e7b2912e597b3ed1728dae8fa
Teun van Zon <teun2408@gmail.com>
2018-01-18 16:55:11 +0100

    Sport komt in reserv

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,6 @@
 
 export const environment = {
   production: false,
-  serverUrl: 'http://localhost:50062/api',
+  serverUrl: 'http://localhost:60701/api',
   serverMongoUrl: 'http://localhost:3000/api/v1'
 };
