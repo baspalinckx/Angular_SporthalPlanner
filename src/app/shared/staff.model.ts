f90be64c040b61e67196b3d585d2274f576d389f
Jeffry <jeffry-1997@outlook.com>
2018-01-18 10:37:11 +0100

    Aanpassingen

--- src/app/shared/staff.model.ts
+++ src/app/shared/staff.model.ts
@@ -4,11 +4,74 @@ export class Staff {
   private _email: string;
   private _startTime: string;
   private _endTime: string;
+  private _mondayAvailabilty: string;
+  private _tuesdayAvailabilty: string;
+  private _wednesdayAvailabilty: string;
+  private _thursdayAvailabilty: string;
+  private _fridayAvailabilty: string;
+  private _saturdayAvailabilty: string;
+  private _sundayAvailabilty: string;
   private _phoneNumber: string;
   private _job: string;
 
   constructor(values: Object = {}) {
     Object.assign(this, values);
+  }
+
+  public get mondayAvailabilty(): string {
+    return this._mondayAvailabilty;
+  }
+
+  public set mondayAvailabilty(value: string) {
+    this._mondayAvailabilty = value;
+  }
+
+  public get tuesdayAvailabilty(): string {
+    return this._tuesdayAvailabilty;
+  }
+
+  public set tuesdayAvailabilty(value: string) {
+    this._tuesdayAvailabilty = value;
+  }
+
+  public get wednesdayAvailabilty(): string {
+    return this._wednesdayAvailabilty;
+  }
+
+  public set wednesdayAvailabilty(value: string) {
+    this._wednesdayAvailabilty = value;
+  }
+
+  public get thursdayAvailabilty(): string {
+    return this._thursdayAvailabilty;
+  }
+
+  public set thursdayAvailabilty(value: string) {
+    this._thursdayAvailabilty = value;
+  }
+
+  public get fridayAvailabilty(): string {
+    return this._fridayAvailabilty;
+  }
+
+  public set fridayAvailabilty(value: string) {
+    this._fridayAvailabilty = value;
+  }
+
+  public get saturdayAvailabilty(): string {
+    return this._saturdayAvailabilty;
+  }
+
+  public set saturdayAvailabilty(value: string) {
+    this._saturdayAvailabilty = value;
+  }
+
+  public get sundayAvailabilty(): string {
+    return this._sundayAvailabilty;
+  }
+
+  public set sundayAvailabilty(value: string) {
+    this._sundayAvailabilty = value;
   }
 
   public get firstName(): string {
